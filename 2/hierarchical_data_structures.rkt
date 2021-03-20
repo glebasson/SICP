8fccd82cb8a1553a5974c7989cd6a7cec7020d45
glebg <glebg@synopsys.com>
2021-03-21 00:07:59 +0300

    fringe

---
+++ 2/hierarchical_data_structures.rkt
@@ -0,0 +1,46 @@
+#lang racket
+(define x (cons (list 1 2)
+                (cons (list 3 4) (list))))
+
+(define (count-leaves tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree)) 1)
+        (else (+ (count-leaves (car tree))
+                 (count-leaves (cdr tree))))))
+(count-leaves x)
+(list 1 (list 2 (list 3 4)))
+(define xx (list 1 2 3))
+(define y (list 4 5 6))
+(append xx y)
+(cons xx y)
+(list xx y)
+
+
+;(reverse (list 1 2 3 4))
+; exercise 2.27
+(define X (list (list 1 2) (list 3 4)))
+(define (deep-reverse tree)
+  (if (pair? tree)
+      (reverse (map deep-reverse tree))
+      tree))
+
+(deep-reverse X)
+(append (list 1 2 3 4 5) (list 2 3 4 5))
+
+; exercise 2.28
+(define nil (quote ()))
+(define (fringe tree)
+  (cond ((null? tree) nil)
+        ((pair? tree)
+         (append (fringe (car tree)) (fringe (cdr tree))))
+        (else
+         (list tree)))
+  )
+
+(list X X)
+(fringe (list X X))
+
+
+
+
+
